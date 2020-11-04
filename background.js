e7f07fc5167f86651086a32275934ef198eb0669
Vlad Lotysh <gangsta1@i.ua>
2020-11-04 19:28:34 +0200

    Added options page

--- background.js
+++ background.js
@@ -14,16 +14,44 @@ function ImageClick(info, tab) {
 chrome.contextMenus.create({
         "title": "Share",
         "contexts": ["page"],
-        "onclick": PageClick
+        "id": "Share",
 })
 
 chrome.contextMenus.create({
     "title": "Share image",
     "contexts": ["image"],
-    "onclick": ImageClick
+    "id": "Share image",
 })
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 console.log(message.action, message.data);
 sendResponse({'text': 'Links are passed: '+ message.data.length});
 });
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    switch (info.menuItemId) {
+        case 'Share image':
+            ImageClick(info, tab);
+            break;
+        case 'Share':
+            PageClick(info, tab);
+            break;
+        default:
+            alert( "No action" );
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'developer.chrome.com'},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  });
